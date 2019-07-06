665c10d107972792847341aa900a3625add2434d
Shobhit <mittalshobhit07@gmail.com>
2019-07-06 22:49:56 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+ import ReactDOM from 'react-dom';
+ const App = () => (
+   <div>
+      <h1>Hello world -- By Shobhit!!</h1>
+   </div>
+ )
+ ReactDOM.render(<App/>, document.getElementById('root'));
